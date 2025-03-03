eb2699a6fa55124468e6b0aed5246438059d9263
Emre Ali Cönk <101756454+EmreAliConk@users.noreply.github.com>
2025-03-03 14:36:54 +0300

    Rules sague screen is done

--- LuckGame/LuckGame/CardSelectionVc.swift
+++ LuckGame/LuckGame/CardSelectionVc.swift
@@ -7,15 +7,23 @@
 
 import UIKit
 
-class CardSelectionVc: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class CardSelectionVc: UIViewController           {
+    
+    @IBOutlet var cardImageView: UIView!
+    @IBOutlet var buttons      : [UIView]!
+    
+    override func viewDidLoad()                   {
+        super.viewDidLoad    ()
+         
+        for button in buttons                     {
+            button.layer.cornerRadius = 8
+        }
 
        
     }
     
-
+    @IBAction func stopButtunTapped(_ sender: Any) {
+    }
     
 
 }
